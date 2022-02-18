d619ec040c8be800de79d7547f9a9f722599e354
LuisGerardoNR <luisgerardonr@gmail.com>
2022-02-17 19:33:42 -0600

    Proyecto inicializado

---
+++ src/proyectogit/ProyectoGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectogit;
+
+/**
+ *
+ * @author lgnra
+ */
+public class ProyectoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        
+        //Agrega tus cambios depues de este comentario
+    }
+    
+}
